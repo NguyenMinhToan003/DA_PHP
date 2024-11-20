4ff56282fd82c9251f4380d02e587f86dc067a63
Toan <nguyentoan04.0003@email.com>
2024-11-20 18:24:36 +0700

    product detail , list

--- index.php
+++ index.php
@@ -12,20 +12,19 @@ $indexBanner = $_GET['indexbanner'] ?? 0;
 $banner = array(
   array(
     'color' => 'black',
-    'title' => 'Black',
+    'title' => 'iPhone 16 Pro Max | Chinh hang VN/A giảm giá 30%',
+    'img' => './images/banner1.png',
   ),
   array(
-    'color' => 'green-900',
+    'color' => 'slate-400',
     'title' => 'Stone',
+
   ),
   array(
-    'color' => 'orange-950',
+    'color' => 'orange-800',
     'title' => 'Orange',
-  ),
-  array(
-    'color' => 'emerald-800',
-    'title' => 'Emerald',
-  ),
+
+  )
 );
 
 ?>
@@ -34,85 +33,99 @@ $banner = array(
 
   <div>
     <div>
-      <div class='flex w-[1170px] justify-between mx-auto py-[7px] items-center my-[8px]'>
-        <div class='font-bold text-[24px] w-[271px]'>Exclusive</div>
-        <div class='flex gap-5'>
-          <div class='text-[16px] font-medium'>Home</div>
-          <div class='text-[16px] font-medium'>Contact</div>
-          <div class='text-[16px] font-medium'>About</div>
-          <div class='text-[16px] font-medium'>Sign Up</div>
-        </div>
-        <div class='relative w-[300px]'>
-          <input placeholder='What are you looking for?'
-            class='bg-gray-50 border border-gray-300 block w-full py-[10px] 
-            pl-[10px] pr-[40px] text-md text-[#db4444]' />
-          <div class='absolute top-0 right-0 h-full w-[50px] flex items-center justify-center'>
-            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-              <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
-            </svg>
+      <div class=' bg-black text-white sticky top-0 z-50 shadow-xl'>
+        <div class='w-[1200px] mx-auto flex justify-between items-center py-2 '>
+          <div class=' font-bold text-[24px] w-[271px]'>Exclusive</div>
+          <div class='flex gap-5'>
+            <div class='text-[16px] font-medium'><a href='/'>Home</a></div>
+            <div class='text-[16px] font-medium'><a href='/product'>Product</a></div>
+            <div class='text-[16px] font-medium'>About</div>
+            <div class='text-[16px] font-medium'>Sign Up</div>
           </div>
-        </div>
-        <button class='btn p-3 bg-slate-400 rounded'>
-          <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312" />
-          </svg>
-
-        </button>
-      </div>
-      <div class=' h-[1px] w-full bg-[#dedede]'>
-        <div class='flex w-[1170px] mx-auto'>
-          <div class='w-[271px] h-full mt-[40px] mr-[16px] flex flex-col gap-4'>
-            <div class='text-[16px] '><a href='#'>Woman’s Fashion</a></div>
-            <div class='text-[16px] '><a href='#'>Man’s Fashion</a></div>
-            <div class='text-[16px] '>Electronics</div>
-            <div class='text-[16px] '>Home & Lifestyle</div>
-            <div class='text-[16px] '>Medicine</div>
-            <div class='text-[16px] '>Sports & Outdoor</div>
-            <div class='text-[16px] '>Baby’s & Toys</div>
-            <div class='text-[16px] '>Groceries & Pets</div>
-            <div class='text-[16px] '>Health & Beauty</div>
-          </div>
-          <div class='h-[388px] w-[1px] bg-[#dedede]'></div>
-          <div class='mt-[40px] ml-[45px] relative'>
-            <div class=' w-[892px] h-[344px] bg-gray-500 overflow-x-hidden  '>
-              <div class='w-full h-full flex banner transition-all  '>
-                <?php
-                foreach ($banner as $key => $value) {
-                  echo "<div class='min-w-full text-white h-full bg-" . $value['color'] . "'>
-                   <div>" . $value['title'] . "</div>
-                  </div>";
-                }
-                ?>
-              </div>
-            </div>
-            <div class=' absolute bottom-[11px] left-[50%] translate-x-[-50%] '>
-              <?php
-              for ($i = 0; $i < count($banner); $i++) {
-              ?>
-                <div class='dot w-3 h-3 cursor-pointer bg-[#808080] rounded-full inline-block mx-1'></div>
-              <?php
-              }
-              ?>
+          <div class='relative w-[300px]'>
+            <input placeholder='What are you looking for?'
+              class='bg-gray-50 border border-gray-300 block w-full py-2
+            pl-2  pr-10 text-md text-primary' />
+            <div class='absolute top-0 right-0 h-full w-[50px] flex items-center justify-center'>
+              <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
+              </svg>
             </div>
           </div>
+          <button class='btn p-3 bg-slate-400 rounded'>
+            <img src='./icons/search.svg' />
+          </button>
         </div>
       </div>
+      <div class=' h-[1px] w-full bg-more'></div>
 
+      <?php
+      $path = $_SERVER['REQUEST_URI'];
+      if ($path == '/') {
+        include 'banner.php';
+        include 'listProduct.php';
+      }
+      if ($path == '/product') {
+        include 'product.php';
+      }
+      ?>
     </div>
   </div>
+
+  </div>
   <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            primary: '#e01020',
+            secondary: '#f2f2f2',
+            more: '#dedede'
+          },
+        },
+      },
+    };
     const dots = document.querySelectorAll('.dot');
     const banner = document.querySelector('.banner');
     let activeDot = dots[0];
-    dots[0].classList.add('bg-[#db4444]', 'border-while', 'border-2');
+    dots[0].classList.add('bg-primary', 'border-while', 'border-2');
     dots.forEach((dot, index) => {
       dot.addEventListener('click', () => {
         banner.style.transform = `translateX(-${index * 100}%)`;
-        activeDot.classList.remove('bg-[#db4444]', 'border-while', 'border-2');
-        dot.classList.add('bg-[#db4444]', 'border-while', 'border-2');
+        activeDot.classList.remove('bg-primary', 'border-while', 'border-2');
+        dot.classList.add('bg-primary', 'border-while', 'border-2');
         activeDot = dot;
       });
     });
+    const arrowLeft = document.querySelector('.arrowlef');
+    const arrowRight = document.querySelector('.arrowright');
+    let indexBanner = 0;
+    const handlerNextBanner = (element = '') => {
+      if (element === 'right' || element === '') {
+        indexBanner++;
+        if (indexBanner >= dots.length) {
+          indexBanner = 0;
+        }
+      } else {
+        indexBanner--;
+        if (indexBanner < 0) {
+          indexBanner = dots.length - 1;
+        }
+      }
+      banner.style.transform = `translateX(-${indexBanner * 100}%)`;
+      activeDot.classList.remove('bg-primary', 'border-while', 'border-2');
+      dots[indexBanner].classList.add('bg-primary', 'border-while', 'border-2');
+      activeDot = dots[indexBanner];
+    }
+    arrowRight.addEventListener('click', () => {
+      handlerNextBanner('right');
+    });
+    arrowLeft.addEventListener('click', () => {
+      handlerNextBanner('left');
+    })
+    const timeout = setInterval(() => {
+      handlerNextBanner();
+    }, 4000);
   </script>
 </body>
 
---
+++ product.php
@@ -0,0 +1,105 @@
+<div class='w-[1200px] mx-auto'>
+  <div class='font-semibold text-lg mt-3'>Samsung Galaxy S24 FE 5G 8GB 128GB </div>
+  <div class='w-full h-[2px] bg-more my-3'></div>
+  <div class='flex items-start gap-6'>
+    <div class='w-7/12 flex flex-col gap-4'>
+      <div class='h-96 rounded-md border-2 border-more shadow-lg'>
+        <img src='https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/s/a/samsung-s23-ulatra_2_.png' class='w-full h-full object-scale-down' />
+      </div>
+      <div class='flex gap-2  overflow-y-auto overflow-x-hidden'>
+        <div class='min-w-20 shadow-md rounded-md h-20 bg-black'></div>
+        <div class='min-w-20 shadow-md rounded-md h-20 bg-black'></div>
+        <div class='min-w-20 shadow-md rounded-md h-20 bg-black'></div>
+        <div class='min-w-20 shadow-md rounded-md h-20 bg-black'></div>
+        <div class='min-w-20 shadow-md rounded-md h-20 bg-black'></div>
+        <div class='min-w-20 shadow-md rounded-md h-20 bg-black'></div>
+        <div class='min-w-20 shadow-md rounded-md h-20 bg-black'></div>
+        <div class='min-w-20 shadow-md rounded-md h-20 bg-black'></div>
+      </div>
+    </div>
+    <div class=' w-5/12 flex flex-col justify-between h-96'>
+      <div class=' flex flex-col gap-3'>
+        <div class='text-[16px] font-medium'>Chọn màu để xem giá và chi nhánh có hàng</div>
+        <div class=' flex flex-wrap gap-2'>
+          <div class=' p-2 border-[2px] border-more rounded-md w-fit'>
+            <div class='text-[12px] font-medium'>Xanh la</div>
+          </div>
+          <div class='p-2 border-[2px] border-more rounded-md w-fit'>
+            <div class='text-[12px] font-medium'>Xanh la</div>
+          </div>
+          <div class='p-2 border-[2px] border-more rounded-md w-fit'>
+            <div class='text-[12px] font-medium'>Xanh la</div>
+          </div>
+        </div>
+
+        <div class='text-[16px] font-medium'>Chọn phiêu bản dung lượng</div>
+        <div class='flex gap-2 flex-wrap'>
+          <div class='p-2 border-[2px] border-more rounded-md w-fit'>
+            <div class='text-[12px] font-medium'>6GB Ram - 128GB</div>
+          </div>
+          <div class='p-2 border-[2px] border-more rounded-md w-fit'>
+            <div class='text-[12px] font-medium'>6GB Ram - 256GB</div>
+          </div>
+          <div class='p-2 border-[2px] border-more rounded-md w-fit'>
+            <div class='text-[12px] font-medium'>12GB Ram - 512GB</div>
+          </div>
+        </div>
+        <div class='text-[16px] font-medium'>Giá sản phẩm</div>
+        <div class='text-[16px] font-bold text-primary '>32.240.000đ</div>
+      </div>
+
+      <div class='flex gap-2 justify-center items-center'>
+        <button class='bg-primary text-white p-2 rounded-md btn w-full '>Mua ngay</button>
+        <button class='border border-primary text-primary p-2 rounded-md btn '><img src='./icons/search.svg' /></button>
+      </div>
+    </div>
+  </div>
+  <div class='flex items-start gap-6 mt-9'>
+    <div class='w-7/12  border border-secondary rounded-lg shadow-2xl p-3 mb-7 '>
+      <div class='bg-secondary rounded-lg p-2 w-full'>
+        <div class=' text-primary font-bold text-lg text-center'>Đặc điểm nổi bật của Samsung Galaxy S24 FE 5G 8GB 128GB</div>
+        <ul class='list-disc w-full pl-4'>
+          <li>Màn hình Dynamic AMOLED 120Hz - Mang lại trải nghiệm hình ảnh siêu mượt mà, màu sắc sống động</li>
+          <li>Chip Exynos 2400e (4nm) cung cấp hiệu năng vượt trội, xử lý mượt mà mọi tác vụ từ chơi game đồ họa cao cho đến đa nhiệm ứng dụng.</li>
+          <li>Camera 50MP chuyên nghiệp với tính năng AI vượt trội giúp bạn chụp ảnh chất lượng cao trong mọi điều kiện ánh sáng.</li>
+          <li>Với viên pin dung lượng 4700mAh và công nghệ sạc nhanh 25W - Galaxy S24 FE đảm bảo sử dụng bền bỉ suốt ngày dài.</li>
+        </ul>
+      </div>
+      <div class='bg-secondary mt-3 p-2'>
+        <div class='text-[16px] font-medium'>Đánh giá:</div>
+        <div class='flex gap-1'>
+          <img src='./icons/star.svg' class='w-5 h-5' />
+          <img src='./icons/star.svg' class='w-5 h-5' />
+          <img src='./icons/star.svg' class='w-5 h-5' />
+          <img src='./icons/star.svg' class='w-5 h-5' />
+          <img src='./icons/starLef.svg' class='w-5 h-5' />
+        </div>
+      </div>
+    </div>
+    <div class=' w-5/12 border border-secondary rounded-lg shadow-2xl p-3 mb-7 '>
+      <div class='font-bold text-xl py-1'>Thông số kỹ thuật </div>
+      <table class='w-full bg-secondary'>
+        <tr>
+          <td class='w-1/2 p-3 '>Màn hình</td>
+          <td class='w-1/2 p-3'>6.9 inches</td>
+        </tr>
+        <tr>
+          <td class='w-1/2 p-3'>Bộ nhớ</td>
+          <td class='w-1/2 p-3'>256GB</td>
+        </tr>
+        <tr>
+          <td class='w-1/2 p-3'>Ram</td>
+          <td class='w-1/2 p-3'>8GB</td>
+        </tr>
+        <tr>
+          <td class='w-1/2 p-3'>Camera</td>
+          <td class='w-1/2 p-3'>108MP</td>
+        </tr>
+        <tr>
+          <td class='w-1/2 p-3'>Pin</td>
+          <td class='w-1/2 p-3'>5000mAh</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</div>
